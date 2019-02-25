9197e3c957ff9356dc61f17ac2138b65f94d3549
Gabriel Passos <gabriel.passos@ilegra.com>
2019-02-25 13:35:29 -0300

    :sparkles: add consumer

---
+++ event-consumer/src/main/java/com/gabrielpassos/event/consumer/EventConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpassos.event.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventConsumerApplication.class, args);
+	}
+
+}
